0648b7938c7823b23f7b5a7c55dccb9cbce86202
Chris-Newton2000 <95302476+Chris-Newton2000@users.noreply.github.com>
2021-12-17 13:06:22 +0100

    Add files via upload

---
+++ speedo.lua
@@ -0,0 +1,18 @@
+function text(content) 
+    SetTextFont(2)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.9,0.7)
+end
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(1)
+        local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*2.2369)
+        if(IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            text(math.floor(speed))
+        end
+    end
+end)
